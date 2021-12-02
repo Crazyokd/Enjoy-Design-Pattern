c559508c7c0ade77fac81ce92b59c8792e30342c
Rekord <1324596506@qq.com>
2021-12-02 21:16:59 +0800

    补充访问者模式的demo

---
+++ 行为模式/访问者(visitor)/demo/visitor/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 访问者模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ObjectStructure o = new ObjectStructure();
+            o.Attach(new ConcreteElementA());
+            o.Attach(new ConcreteElementB());
+
+            ConcreteVisitor1 v1 = new ConcreteVisitor1();
+            ConcreteVisitor2 v2 = new ConcreteVisitor2();
+
+            o.Accept(v1);
+            o.Accept(v2);
+
+            Console.Read();
+        }
+    }
+
+    //要求Element子类数量较稳定，这样才可以使得Vistor抽象类具有稳定的结构
+    abstract class Visitor
+    {
+        public abstract void VisitConcreteElementA(Element concreteElementA);
+
+        public abstract void VisitConcreteElementB(Element concreteElementB);
+    }
+
+    class ConcreteVisitor1 : Visitor
+    {
+        public override void VisitConcreteElementA(Element concreteElementA)
+        {
+            Console.WriteLine("{0}被{1}访问", concreteElementA.GetType().Name, this.GetType().Name);
+        }
+
+        public override void VisitConcreteElementB(Element concreteElementB)
+        {
+            Console.WriteLine("{0}被{1}访问", concreteElementB.GetType().Name, this.GetType().Name);
+        }
+    }
+
+    class ConcreteVisitor2 : Visitor
+    {
+        public override void VisitConcreteElementA(Element concreteElementA)
+        {
+            Console.WriteLine("{0}被{1}访问", concreteElementA.GetType().Name, this.GetType().Name);
+        }
+
+        public override void VisitConcreteElementB(Element concreteElementB)
+        {
+            Console.WriteLine("{0}被{1}访问", concreteElementB.GetType().Name, this.GetType().Name);
+        }
+    }
+
+    abstract class Element
+    {
+        public abstract void Accept(Visitor visitor);
+    }
+
+    class ConcreteElementA : Element
+    {
+        public override void Accept(Visitor visitor)
+        {
+            visitor.VisitConcreteElementA(this);
+        }
+
+        // 用于定义类的其他操作
+        public void OperationA()
+        { }
+    }
+
+    class ConcreteElementB : Element
+    {
+        public override void Accept(Visitor visitor)
+        {
+            visitor.VisitConcreteElementB(this);
+        }
+        
+        public void OperationB()
+        { }
+    }
+
+    // 为客户端提供一个高层接口  
+    class ObjectStructure
+    {
+        private IList<Element> elements = new List<Element>();
+
+        public void Attach(Element element)
+        {
+            elements.Add(element);
+        }
+
+        public void Detach(Element element)
+        {
+            elements.Remove(element);
+        }
+
+        public void Accept(Visitor visitor)
+        {
+            foreach (Element e in elements)
+            {
+                e.Accept(visitor);
+            }
+        }
+    }
+}
